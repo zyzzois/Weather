e73fbc62369e6268d82806dace181bb804d21183
Octane Silvio <ruslangibadullin70@gmail.com>
2022-11-08 23:58:37 +0500

    added constants

---
+++ app/src/main/java/com/octaneocatane/weather/utils/Constants.kt
@@ -0,0 +1,11 @@
+package com.octaneocatane.weather.utils
+
+object Constants {
+
+    const val BASE_URL = "https://api.weatherapi.com"
+    const val UNDEFINED_CITY = "default_city"
+    const val UNDEFINED_MAX_TEMP = "max_temp"
+    const val UNDEFINED_MIN_TEMP = "min_temp"
+    const val UNDEFINED_CURRENT_TEMP = "current_temp"
+
+}
