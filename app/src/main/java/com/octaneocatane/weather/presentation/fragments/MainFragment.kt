f8b928ca72b3cf023992a1f07e7e4cc653f78f6a
Octane Silvio <ruslangibadullin70@gmail.com>
2022-11-09 22:17:49 +0500

    added permission listeners for GPS location and we can get current lat/lon and push it to loadData()

--- app/src/main/java/com/octaneocatane/weather/presentation/fragments/MainFragment.kt
+++ app/src/main/java/com/octaneocatane/weather/presentation/fragments/MainFragment.kt
@@ -1,23 +1,39 @@
 package com.octaneocatane.weather.presentation.fragments
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationTokenSource
 import com.google.android.material.tabs.TabLayoutMediator
-import com.octaneocatane.api.presentation.ViewPager2Adapter
-import com.octaneocatane.weather.R
+import com.octaneocatane.weather.presentation.ViewPager2Adapter
 import com.octaneocatane.weather.databinding.FragmentMainBinding
 import com.octaneocatane.weather.presentation.DialogManager
 import com.octaneocatane.weather.presentation.MainViewModel
+import com.octaneocatane.weather.utils.isPermissionGranted
 import com.squareup.picasso.Picasso
 
 class MainFragment : Fragment() {
+    private val fLocationClient by lazy {
+        LocationServices.getFusedLocationProviderClient(requireContext())
+    }
+    private lateinit var permissionLauncher: ActivityResultLauncher<String>
 
     private var _binding: FragmentMainBinding? = null
     private val binding: FragmentMainBinding
@@ -32,16 +48,7 @@ class MainFragment : Fragment() {
     }
 
     private val fragmentList = listOf(HoursFragment.newInstance(), DaysFragment.newInstance())
-    private val tabList = listOf("Hours", "Days")
-
-    private lateinit var pLauncher: ActivityResultLauncher<String>
-
-    private val fLocationClient by lazy {
-        LocationServices.getFusedLocationProviderClient(requireContext())
-    }
-
-    //private lateinit var fLocationClient: FusedLocationProviderClient
-
+    private val tabList = listOf(HOURS, DAYS)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -53,27 +60,81 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //checkPermission()
+        checkPermission()
         init()
         updateCurrentCard()
-        //getLocation()
+    }
+
+    private fun checkLocation() {
+        if (isLocationEnabled()) {
+            getLocation()
+        } else {
+            DialogManager.locationSettingsDialog(requireContext(), object : DialogManager.Listener {
+                override fun onClick(name: String?) {
+                    startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+                }
+            })
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val lm = activity?.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val locationEnabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        if (!locationEnabled) {
+            Toast.makeText(requireContext(), "Location disabled!", Toast.LENGTH_LONG).show()
+            return false
+        } else {
+            return true
+        }
+    }
+
+    private fun getLocation() {
+        val ct = CancellationTokenSource()
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        fLocationClient
+            .getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, ct.token)
+            .addOnCompleteListener {
+                val coordinates = "${it.result.latitude},${it.result.longitude}"
+                viewModel.loadData(coordinates)
+                Log.d("TAG", "${it.result.latitude},${it.result.longitude}")
+            }
+    }
+
+    private fun checkPermission() {
+        if (!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)) {
+            permissionLauncher = registerForActivityResult(
+                ActivityResultContracts.RequestPermission()) {
+                Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
+            }
+            permissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
 
     private fun init() = with(binding) {
         viewPager2.adapter = viewPagerAdapter
 
-        TabLayoutMediator(tabLayout, viewPager2){
-                tab, pos -> tab.text = tabList[pos]
+        TabLayoutMediator(tabLayout, viewPager2) { tab, pos ->
+            tab.text = tabList[pos]
         }.attach()
 
         buttonSyncLocation.setOnClickListener {
             tabLayout.selectTab(tabLayout.getTabAt(0))
+            checkLocation()
         }
 
         binding.buttonTypeCity.setOnClickListener {
-            DialogManager.searchByNameDialog(requireContext(), object : DialogManager.Listener{
+            DialogManager.searchByNameDialog(requireContext(), object : DialogManager.Listener {
                 override fun onClick(name: String?) {
-                    name?.let { /*it1 -> requestWeatherData(it1)*/ }
+                    name?.let { viewModel.loadData(it) }
                 }
             })
         }
@@ -85,10 +146,15 @@ class MainFragment : Fragment() {
             tvCurrentTemp.text = it.currentTemp
             tvCurrentCondition.text = it.conditionText
             tvLastUpdated.text = it.time
-            val tempText = "${it.maxTemp} - max | min - ${it.minTemp}"
+            val tempText = "${it.maxTemp} °C | ${it.minTemp} °C"
             tvMaxMinTemp.text = tempText
             Picasso.get().load("https:" + it.conditionIcon).into(imWeather)
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        checkLocation()
     }
 
     override fun onDestroyView() {
@@ -97,6 +163,8 @@ class MainFragment : Fragment() {
     }
 
     companion object {
+        private const val HOURS = "hours"
+        private const val DAYS = "days"
         @JvmStatic
         fun newInstance() = MainFragment()
     }
